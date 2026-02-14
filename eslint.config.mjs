24b783f8574c79f283bb73ac496cea86b82657bf
cylewaitforit <cyle@cylewaitfor.it>
2026-02-14 07:52:52 -0600

    chore: add eslint-plugin-dependabot

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import nx from '@nx/eslint-plugin';
+import dependabot from "eslint-plugin-dependabot";
 import packageJson from 'eslint-plugin-package-json';
 import perfectionist from 'eslint-plugin-perfectionist';
+import { yaml } from "eslint-yaml";
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
@@ -121,4 +123,14 @@ export default defineConfig([
       'package-json/require-engines': 'error',
     },
   },
+  	{
+		name: "dependabot config",
+		extends: [dependabot.configs.recommended],
+		files: ["**/.github/dependabot.{yml,yaml}"],
+		language: "yaml/yaml",
+		plugins: {
+			dependabot,
+			yaml,
+		},
+	},
 ]);
